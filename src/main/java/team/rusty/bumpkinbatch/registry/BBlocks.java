75d6c4e15a12932f197326c4f78f00ff8afff536
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2021-10-08 21:13:44 -0700

    Blocks deferred register

--- src/main/java/team/rusty/bumpkinbatch/registry/BBlocks.java
+++ src/main/java/team/rusty/bumpkinbatch/registry/BBlocks.java
@@ -1,5 +1,12 @@
 package team.rusty.bumpkinbatch.registry;
 
-public class BBlocks {
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import team.rusty.bumpkinbatch.BumpkinBatch;
+
+public final class BBlocks {
+    public static final DeferredRegister<Item> BLOCKS = DeferredRegister.create(ForgeRegistries.ITEMS, BumpkinBatch.ID);
+
 
 }
