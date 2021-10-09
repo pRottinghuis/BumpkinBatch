fbf6b1979f29f0d92e647fdb0f6c23aaa8adc0df
cfrishausen <p.rttnghs@outlook.com>
2021-10-09 13:22:09 -0700

    Reaper entity

--- src/main/java/team/rusty/bumpkinbatch/entity/ReaperEntity.java
+++ src/main/java/team/rusty/bumpkinbatch/entity/ReaperEntity.java
@@ -1,4 +1,43 @@
 package team.rusty.bumpkinbatch.entity;
 
-public class ReaperEntity {
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+
+public class ReaperEntity extends Monster {
+    protected ReaperEntity(EntityType<? extends Monster> entityType, Level level) {
+        super(entityType, level);
+
+        public void registerGoals() {
+
+        }
+
+        public void addBehaviorGoals() {
+
+        }
+
+        public static AttributeSupplier.Builder createAttributes() {
+
+        }
+
+
+        public void defineSynchedData() {
+
+        }
+
+        public int getExperienceReward(Player player) {
+
+        }
+
+        public void tick() {
+
+        }
+
+        public void aiStep() {
+
+        }
+
+    }
 }
