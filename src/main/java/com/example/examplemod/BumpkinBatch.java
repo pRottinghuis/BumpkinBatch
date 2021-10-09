cb535eb9642eced6f4d10904f6eabdee6c675adb
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2021-10-08 19:18:32 -0700

    Main mod class and mods.toml

---
+++ src/main/java/com/example/examplemod/BumpkinBatch.java
@@ -0,0 +1,25 @@
+package com.example.examplemod;
+
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+@Mod("bumpkinbatch")
+public class BumpkinBatch {
+    public BumpkinBatch() {
+
+    }
+}
