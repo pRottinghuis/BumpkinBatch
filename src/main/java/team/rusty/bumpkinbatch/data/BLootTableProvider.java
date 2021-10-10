302b42d26a3185312965aeed68be90775b70f31d
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2021-10-09 22:40:24 -0700

    Fix loot table generation generating vanilla tables

--- src/main/java/team/rusty/bumpkinbatch/data/BLootTableProvider.java
+++ src/main/java/team/rusty/bumpkinbatch/data/BLootTableProvider.java
@@ -4,12 +4,19 @@ import com.google.common.collect.Maps;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.data.DataProvider;
+import net.minecraft.data.HashCache;
 import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.storage.loot.LootTable;
+import net.minecraft.world.level.storage.loot.LootTables;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Map;
 
 public class BLootTableProvider extends LootTableProvider {
@@ -21,5 +28,33 @@ public class BLootTableProvider extends LootTableProvider {
     public BLootTableProvider(DataGenerator generator) {
         super(generator);
         this.generator = generator;
+    }
+
+    @Override
+    public void run(HashCache cache) {
+        // add tables
+
+        HashMap<ResourceLocation, LootTable> namespacedTables = new HashMap<>(blockTables.size());
+
+        for (Map.Entry<Block, LootTable.Builder> entry : blockTables.entrySet()) {
+            // Add tables to the block loot parameter set automatically
+            namespacedTables.put(entry.getKey().getLootTable(), entry.getValue().setParamSet(LootContextParamSets.BLOCK).build());
+        }
+
+        writeLootTables(namespacedTables, cache);
+    }
+
+    private void writeLootTables(Map<ResourceLocation, LootTable> tables, HashCache cache) {
+        Path outputFolder = generator.getOutputFolder();
+
+        for (Map.Entry<ResourceLocation, LootTable> entry : tables.entrySet()) {
+            Path path = outputFolder.resolve("data/" + entry.getKey().getNamespace() + "/loot_tables/" + entry.getKey().getPath() + ".json");
+
+            try {
+                DataProvider.save(GSON, cache, LootTables.serialize(entry.getValue()), path);
+            } catch (Exception e) {
+                LOGGER.error("Couldn't write loot table {}", path, e);
+            }
+        }
     }
 }
