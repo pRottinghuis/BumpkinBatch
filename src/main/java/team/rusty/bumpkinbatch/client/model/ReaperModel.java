d87c0b8c1b8d97dba005e57db2a33431923a1bce
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2021-10-09 22:23:41 -0700

    Optimize reaper texture

--- src/main/java/team/rusty/bumpkinbatch/client/model/ReaperModel.java
+++ src/main/java/team/rusty/bumpkinbatch/client/model/ReaperModel.java
@@ -52,12 +52,12 @@ public class ReaperModel extends EntityModel<ReaperEntity> {
                 PartPose.rotation(0.0F, 12.0F, 0.0F));
 
         var body = waist.addOrReplaceChild("body", CubeListBuilder.create()
-                .texOffs(0, 64).addBox(-5.0F, -15.0F, -2.0F, 10.0F, 18.0F, 4.0F)
-                .texOffs(60, 60).addBox(-5.0F, -15.0F, -2.0F, 10.0F, 18.0F, 4.0F, new CubeDeformation(0.5F)),
+                .texOffs(0, 22).addBox(-5.0F, -15.0F, -2.0F, 10.0F, 18.0F, 4.0F)
+                .texOffs(0,  0).addBox(-5.0F, -15.0F, -2.0F, 10.0F, 18.0F, 4.0F, new CubeDeformation(0.5F)),
                 PartPose.rotation(0.0F, -12.0F, 0.0F));
 
         var head = body.addOrReplaceChild("head", CubeListBuilder.create()
-                .texOffs(28, 64).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 9.0F, 8.0F),
+                .texOffs(28, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 9.0F, 8.0F),
                 PartPose.rotation(0.0F, -16.0F, 0.0F));
 
         var hat = head.addOrReplaceChild("hat", CubeListBuilder.create()
@@ -65,25 +65,25 @@ public class ReaperModel extends EntityModel<ReaperEntity> {
                 PartPose.rotation(0.0F, 16.0F, 0.0F));
 
         var rightArm = body.addOrReplaceChild("right_arm", CubeListBuilder.create()
-                .texOffs(52, 81).addBox(-3.0F, 0.0F, -1.0F, 2.0F, 20.0F, 2.0F)
-                .texOffs(0, 32).addBox(-4.0F, 0.0F, -2.0F, 3.0F, 9.0F, 4.0F, new CubeDeformation(0.5F)),
+                .texOffs(36, 30).mirror().addBox(-3.0F, 0.0F, -1.0F, 2.0F, 20.0F, 2.0F)
+                .texOffs(46, 17).mirror().addBox(-4.0F, 0.0F, -2.0F, 3.0F, 9.0F, 4.0F, new CubeDeformation(0.5F)),
                 PartPose.rotation(-5.0F, -15.0F, 0.0F));
 
         var leftArm = body.addOrReplaceChild("left_arm", CubeListBuilder.create()
-                .texOffs(44, 81).addBox(1.0F, 0.0F, -1.0F, 2.0F, 20.0F, 2.0F)
-                .texOffs(0, 0).addBox(1.0F, 0.0F, -2.0F, 3.0F, 9.0F, 4.0F, new CubeDeformation(0.5F)),
+                .texOffs(36, 30).addBox(1.0F, 0.0F, -1.0F, 2.0F, 20.0F, 2.0F)
+                .texOffs(46, 17).addBox(1.0F, 0.0F, -2.0F, 3.0F, 9.0F, 4.0F, new CubeDeformation(0.5F)),
                 PartPose.rotation(5.0F, -15.0F, 0.0F));
 
         var rightLeg = body.addOrReplaceChild("right_leg", CubeListBuilder.create()
-                .texOffs(36, 81).addBox(-2.0F, 2.0F, -1.0F, 2.0F, 21.0F, 2.0F)
-                .texOffs(48, 0).addBox(-3.0F, 2.0F, -2.0F, 5.0F, 9.0F, 4.0F, new CubeDeformation(0.5F))
-                .texOffs(60, 82).addBox(-2.0F, 2.0F, -1.0F, 2.0F, 13.0F, 2.0F, new CubeDeformation(0.5F)),
+                .texOffs(28, 30).mirror().addBox(-2.0F, 2.0F, -1.0F, 2.0F, 21.0F, 2.0F)
+                .texOffs(28, 17).mirror().addBox(-3.0F, 2.0F, -2.0F, 5.0F, 9.0F, 4.0F, new CubeDeformation(0.5F))
+                .texOffs(44, 30).mirror().addBox(-2.0F, 2.0F, -1.0F, 2.0F, 13.0F, 2.0F, new CubeDeformation(0.5F)),
                 PartPose.rotation(-2.0F, 1.0F, 0.0F));
 //
         var leftLeg = body.addOrReplaceChild("left_leg", CubeListBuilder.create()
-                .texOffs(28, 81).addBox(0.0F, 2.0F, -1.0F, 2.0F, 21.0F, 2.0F)
-                .texOffs(48, 0).addBox(-2.0F, 2.0F, -2.0F, 5.0F, 9.0F, 4.0F, new CubeDeformation(0.5F))
-                .texOffs(60, 82).addBox(0.0F, 2.0F, -1.0F, 2.0F, 13.0F, 2.0F, new CubeDeformation(0.5F)),
+                .texOffs(28, 30).addBox(0.0F, 2.0F, -1.0F, 2.0F, 21.0F, 2.0F)
+                .texOffs(28, 17).addBox(-2.0F, 2.0F, -2.0F, 5.0F, 9.0F, 4.0F, new CubeDeformation(0.5F))
+                .texOffs(44, 30).addBox(0.0F, 2.0F, -1.0F, 2.0F, 13.0F, 2.0F, new CubeDeformation(0.5F)),
                 PartPose.rotation(2.0F, 1.0F, 0.0F));
 
         //mesh, height, width
